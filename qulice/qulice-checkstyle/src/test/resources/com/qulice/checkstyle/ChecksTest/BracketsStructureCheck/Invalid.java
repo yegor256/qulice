89d8e5aec8cde62577457f2bea2268591f8cea70
Dmitry Bashkin <dmitry.bashkin@gmail.com>
2011-11-04 09:53:44 +0000

    refs #32 - todo added.

--- qulice/qulice-checkstyle/src/test/resources/com/qulice/checkstyle/ChecksTest/BracketsStructureCheck/Invalid.java
+++ qulice/qulice-checkstyle/src/test/resources/com/qulice/checkstyle/ChecksTest/BracketsStructureCheck/Invalid.java
@@ -3,6 +3,25 @@
  * only as a text resource in integration.ChecksIT.
  */
 public final class Invalid {
+/*
+    @todo #123! Realize method and constructor declaration checking.
+    public Invalid(String name,
+     String value
+     ) {
+    }
+    public Invalid(
+        String name,
+        String value) {
+    }
+    public void print(String format,
+       String text
+    ) {
+    }
+    public void print(
+        String format,
+        String text) {
+    }
+*/
     public void main() {
       String.format("File %s not found",
         file);
