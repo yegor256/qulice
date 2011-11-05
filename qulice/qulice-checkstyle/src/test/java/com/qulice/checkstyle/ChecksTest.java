37abd15ecfba304467e76f76b2ce0597556cce8a
Dmitry Bashkin <dmitry.bashkin@gmail.com>
2011-11-05 16:18:41 +0000

    refs #40 - done.

--- qulice/qulice-checkstyle/src/test/java/com/qulice/checkstyle/ChecksTest.java
+++ qulice/qulice-checkstyle/src/test/java/com/qulice/checkstyle/ChecksTest.java
@@ -71,6 +71,7 @@ public final class ChecksTest {
         "CascadeIndentationCheck",
         "BracketsStructureCheck",
         "ConstantUsageCheck",
+        "JavadocLocationCheck",
     };
 
     /**
