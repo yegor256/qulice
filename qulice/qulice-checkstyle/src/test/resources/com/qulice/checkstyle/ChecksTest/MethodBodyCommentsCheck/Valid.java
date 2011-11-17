ac2505628b4fa25bde51b5c2e3ed6d1f5c918129
Dmitry Bashkin <dmitry.bashkin@gmail.com>
2011-11-17 14:55:02 +0000

    refs #46 - done.

---
+++ qulice/qulice-checkstyle/src/test/resources/com/qulice/checkstyle/ChecksTest/MethodBodyCommentsCheck/Valid.java
@@ -0,0 +1,22 @@
+/**
+ * This is not a real Java class. It won't be compiled ever. It is used
+ * only as a text resource in integration.ChecksIT.
+ */
+public final class Valid {
+    /**
+     * Comments.
+     */
+    public Invalid(String name, String value) {
+        this.a = b;
+        // @checkstyle MagicNumber (1 line)
+        this.a = b;
+    }
+    /**
+     * Comments.
+     */
+    public void print(String format, String text) {
+        int c = 0;
+        // @checkstyle MagicNumber (1 line)
+        this.a = b;
+    }
+}
