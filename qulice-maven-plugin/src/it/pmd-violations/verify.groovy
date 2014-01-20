9bc1ad5e7d3817e2fb5dc63a02cecef23f174d60
krzyk <Krzysztof.Krason@sabre.com>
2014-01-20 12:40:30 +0100

    #105 recreated problem with local variables

--- qulice-maven-plugin/src/it/pmd-violations/verify.groovy
+++ qulice-maven-plugin/src/it/pmd-violations/verify.groovy
@@ -32,5 +32,9 @@
  */
 
 def log = new File(basedir, 'build.log')
+assert log.text.contains('2 PMD violation')
 assert log.text.contains(' PMD violations (see log above)')
 assert log.text.contains("Avoid unused private fields such as 'var'")
+assert log.text.contains("Avoid creating unnecessary local variables like 'name'")
+
+
