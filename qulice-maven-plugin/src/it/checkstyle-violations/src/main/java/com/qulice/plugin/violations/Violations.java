a6c621901e7d4e3dd7c2df619151a26adbc2e31b
krzyk <Krzysztof.Krason@sabre.com>
2014-02-23 11:19:50 +0100

    #136 recreated problem

--- qulice-maven-plugin/src/it/checkstyle-violations/src/main/java/com/qulice/plugin/violations/Violations.java
+++ qulice-maven-plugin/src/it/checkstyle-violations/src/main/java/com/qulice/plugin/violations/Violations.java
@@ -3,4 +3,13 @@ public class Violations {
     public void test() {
         System.setProperty("test", "test value");
     }
+
+    /**
+     * Test method.
+     */
+    public final void foreach() {
+        for (String txt : new String[] {"test"}) {
+            System.out.println(txt);
+        }
+    }
 }
