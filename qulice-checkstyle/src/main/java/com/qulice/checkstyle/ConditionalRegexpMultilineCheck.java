0507cdd50de9eaee29c5639ba57b21453595c28b
krzyk <Krzysztof.Krason@sabre.com>
2014-03-10 09:27:17 +0100

    #80 empty ArrayList initialization is not allowed

---
+++ qulice-checkstyle/src/main/java/com/qulice/checkstyle/ConditionalRegexpMultilineCheck.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2011-2013, Qulice.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the Qulice.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.qulice.checkstyle;
+
+import com.puppycrawl.tools.checkstyle.checks.regexp.RegexpMultilineCheck;
+import java.io.File;
+import java.util.List;
+import java.util.regex.Pattern;
+
+/**
+ * Performs multiline regexp match only if a regexp condition passes.
+ * @author Krzysztof Krason (Krzysztof.Krason@gmail.com)
+ * @version $Id$
+ */
+public final class ConditionalRegexpMultilineCheck extends
+    RegexpMultilineCheck {
+    /**
+     * Condition that has to pass.
+     */
+    private transient Pattern condition = Pattern.compile(".");
+
+    @Override
+    protected void processFiltered(final File file, final List<String> lines) {
+        boolean found = false;
+        for (final String line: lines) {
+            if (this.condition.matcher(line).find()) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            super.processFiltered(file, lines);
+        }
+    }
+
+    /**
+     * Condition regexp that has to match before checking the core one.
+     * @param cond Regexp that has to match in file.
+     */
+    public void setCondition(final String cond) {
+        this.condition = Pattern.compile(cond);
+    }
+}
