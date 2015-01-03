884bd4febe8139882b7d03e7850d19ce156614c7
alevohin <alevohin@mail.ru>
2015-01-04 00:58:36 +0300

    Integration test fixed Checkstyle fixed

--- qulice-ant/src/main/java/com/qulice/ant/AntEnvironment.java
+++ qulice-ant/src/main/java/com/qulice/ant/AntEnvironment.java
@@ -1,10 +1,38 @@
+/**
+ * Copyright (c) 2011-2014, Qulice.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the Qulice.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.qulice.ant;
 
 import com.jcabi.log.Logger;
 import com.qulice.spi.Environment;
 import java.io.File;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.*;
@@ -13,30 +41,50 @@ import org.apache.commons.io.filefilter.DirectoryFileFilter;
 import org.apache.commons.io.filefilter.IOFileFilter;
 import org.apache.commons.io.filefilter.WildcardFileFilter;
 import org.apache.tools.ant.Project;
-import org.apache.tools.ant.types.FileSet;
 import org.apache.tools.ant.types.Path;
-import org.apache.tools.ant.util.CollectionUtils;
 
 /**
- * Created with IntelliJ IDEA.
- * User: alevohins
- * Date: 03.01.15
- * Time: 22:39
- * To change this template use File | Settings | File Templates.
+ * Environment, passed from ant task to validators.
+ * @author Yuriy Alevohin (alevohin@mail.ru)
+ * @version $Id$
  */
-
 public class AntEnvironment implements Environment {
 
+    /**
+     * Ant project.
+     */
     private transient final Project project;
-    private transient final Path sourcepath;
+
+    /**
+     * Sources dirs.
+     */
+    private transient final Path srcdir;
+
+    /**
+     * Classes dir (only one dir is supported)
+     */
+    private transient final java.io.File classesdir;
+
+    /**
+     * Classpath dirs and files.
+     */
     private transient final Path classpath;
 
+    /**
+     * Public constructor.
+     * @param project - ant project
+     * @param srcdir  - sources dirs
+     * @param classesdir - classes dir
+     * @param classpath - classpath
+     */
     public AntEnvironment(
         Project project,
-        Path sourcepath,
+        Path srcdir,
+        java.io.File classesdir,
         Path classpath) {
         this.project = project;
-        this.sourcepath = sourcepath;
+        this.srcdir = srcdir;
+        this.classesdir = classesdir;
         this.classpath = classpath;
     }
 
@@ -52,7 +100,7 @@ public class AntEnvironment implements Environment {
 
     @Override
     public File outdir() {
-        return new File(this.basedir(), "classes");
+        return classesdir;
     }
 
     @Override
@@ -77,12 +125,14 @@ public class AntEnvironment implements Environment {
                 throw new IllegalStateException("Failed to build URL", ex);
             }
         }
+        try {
+            urls.add(classesdir.toURI().toURL());
+        } catch (MalformedURLException ex) {
+            throw new IllegalStateException("Failed to build URL", ex);
+        }
         final URLClassLoader loader = new URLClassLoader(
             urls.toArray(new URL[urls.size()]),
-//            Thread.currentThread().getContextClassLoader()
-            project.getCoreLoader() == null ?
-                Thread.currentThread().getContextClassLoader() :
-                project.getCoreLoader()
+            Thread.currentThread().getContextClassLoader()
         );
         for (final URL url : loader.getURLs()) {
             Logger.debug(this, "Classpath: %s", url);
@@ -99,7 +149,7 @@ public class AntEnvironment implements Environment {
     public Collection<File> files(String pattern) {
         final Collection<File> files = new LinkedList<File>();
         final IOFileFilter filter = new WildcardFileFilter(pattern);
-        for (final String dir : sourcepath.list()) {
+        for (final String dir : srcdir.list()) {
             final File sources = new File(dir);
             if (sources.exists() && sources.isDirectory()) {
                 files.addAll(
--- qulice-ant/src/main/java/com/qulice/ant/QuliceTask.java
+++ qulice-ant/src/main/java/com/qulice/ant/QuliceTask.java
@@ -1,9 +1,39 @@
+/**
+ * Copyright (c) 2011-2014, Qulice.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the Qulice.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.qulice.ant;
 
 import com.jcabi.log.Logger;
 import com.qulice.spi.Environment;
 import com.qulice.spi.ValidationException;
 import com.qulice.spi.Validator;
+import java.io.File;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import org.apache.tools.ant.BuildException;
@@ -11,15 +41,26 @@ import org.apache.tools.ant.Task;
 import org.apache.tools.ant.types.Path;
 
 /**
- * Created with IntelliJ IDEA.
- * User: alevohins
- * Date: 03.01.15
- * Time: 11:49
- * To change this template use File | Settings | File Templates.
+ * Ant Task for Qulice.
+ *
+ * @author Yuriy Alevohin (alevohin@mail.ru)
+ * @version $Id$
  */
 public class QuliceTask extends Task {
 
-    private transient Path sourcepath;
+    /**
+     * Sources dirs.
+     */
+    private transient Path srcdir;
+
+    /**
+     * Classes dir (only one dir is supported)
+     */
+    private transient File classesdir;
+
+    /**
+     * Classpath dirs and files.
+     */
     private transient Path classpath;
 
     @Override
@@ -43,25 +84,43 @@ public class QuliceTask extends Task {
         }
     }
 
+    /**
+     * Create Environment.
+     * @return Environment.
+     * @throws BuildException If ant task doesn't have mandatory params.
+     */
     private Environment environment() throws BuildException {
-        if (sourcepath == null) {
+        if (srcdir == null) {
             throw new BuildException("sourcepath not defined for QuliceTask");
+        }
+        if (classesdir == null) {
+            throw new BuildException("classesdir not defined for QuliceTask");
         }
         if (classpath == null) {
             throw new BuildException("classpath not defined for QuliceTask");
         }
         return new AntEnvironment(
             getProject(),
-            sourcepath,
+            srcdir,
+            classesdir,
             classpath);
     }
 
+    /**
+     * Validate and throws exception if there are any problems.
+     * @param env Environment
+     * @throws ValidationException If there are any problems.
+     */
     private void validate(Environment env) throws ValidationException {
         for (final Validator validator : this.validators()) {
             validator.validate(env);
         }
     }
 
+    /**
+     * Create set of Validators.
+     * @return Set of Validators.
+     */
     private Set<Validator> validators() {
         final Set<Validator> validators = new LinkedHashSet<Validator>();
         validators.add(new com.qulice.checkstyle.CheckstyleValidator());
@@ -72,10 +131,26 @@ public class QuliceTask extends Task {
         return validators;
     }
 
-    public void setSourcepath(Path sourcepath) {
-        this.sourcepath = sourcepath;
+    /**
+     * Set source dirs
+     * @param srcdir Source dirs
+     */
+    public void setSrcdir(Path srcdir) {
+        this.srcdir = srcdir;
     }
 
+    /**
+     * Set classes dir
+     * @param classesdir Classes dir
+     */
+    public void setClassesdir(File classesdir) {
+        this.classesdir = classesdir;
+    }
+
+    /**
+     * Set classpath
+     * @param classpath classpath
+     */
     public void setClasspath(Path classpath) {
         this.classpath = classpath;
     }
