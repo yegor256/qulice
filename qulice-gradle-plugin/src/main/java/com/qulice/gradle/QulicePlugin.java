4c70b88c6be265694cf8029340ca68badf509312
Dmitri Pisarenko <dp@altruix.co>
2015-01-14 21:52:32 +0300

    Added skeletal methods for individual Qulice checks.

--- qulice-gradle-plugin/src/main/java/com/qulice/gradle/QulicePlugin.java
+++ qulice-gradle-plugin/src/main/java/com/qulice/gradle/QulicePlugin.java
@@ -11,6 +11,60 @@ import org.gradle.api.Project;
  */
 public class QulicePlugin implements Plugin<Project> {
     public void apply(Project project) {
-        // Add plugin implementation here
+        this.runMavenDependencyAnalysis(prj);
+        this.runMavenEnforcerPlugin(prj);
+        this.runJsLintMavenPlugin(prj);
+        this.runmavenDuplicateFinderPlugin(prj);
+        this.runCheckstyle(prj);
+        this.runPMD(prj);
+        this.runFindBugs(prj);
+        this.runCobertura(prj);
+        this.runCodeNarc(prj);
+    }
+
+    public void runMavenDependencyAnalysis(Project prj) {
+        /**
+         * @todo
+         **/
+    }
+    public void runMavenEnforcerPlugin(Project prj) {
+        /**
+         * @todo
+         **/
+    }
+    public void runJsLintMavenPlugin(Project prj) {
+        /**
+         * @todo
+         **/
+    }
+    public void runmavenDuplicateFinderPlugin(Project prj) {
+        /**
+         * @todo
+         **/
+    }
+    public void runCheckstyle(Project prj) {
+        /**
+         * @todo
+         **/
+    }
+    public void runPMD(Project prj) {
+        /**
+         * @todo
+         **/
+    }
+    public void runFindBugs(Project prj) {
+        /**
+         * @todo
+         **/
+    }
+    public void runCobertura(Project prj) {
+        /**
+         * @todo
+         **/
+    }
+    public void runCodeNarc(Project prj) {
+        /**
+         * @todo
+         **/
     }
 }
