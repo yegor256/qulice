747d83acb5da7c001416fb70908232a1e65a29d7
Dmitri Pisarenko <dp@altruix.co>
2015-01-14 21:33:41 +0300

    Now QulicePlugin can be built using `gradle build`.

---
+++ qulice-gradle-plugin/src/main/java/com/qulice/gradle/QulicePlugin.java
@@ -0,0 +1,16 @@
+package com.qulice.gradle;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+
+/**
+ * @author Dmitri Pisarenko (dp@altruix.co)
+ * @version $Id$
+ * @since 1.0
+ */
+public class QulicePlugin implements Plugin<Project> {
+    public void apply(Project project) {
+        // Add plugin implementation here
+    }
+}
