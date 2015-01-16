7a48ee0ba67b2571a1bb53962980ecc80e9f270e
Dmitri Pisarenko <dp@altruix.co>
2015-01-16 14:41:09 +0300

    Fixed errors in the todo statements.

--- qulice-gradle-plugin/src/main/java/com/qulice/gradle/QulicePlugin.java
+++ qulice-gradle-plugin/src/main/java/com/qulice/gradle/QulicePlugin.java
@@ -3,7 +3,6 @@ package com.qulice.gradle;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
-
 /**
  * @author Dmitri Pisarenko (dp@altruix.co)
  * @version $Id$
@@ -24,47 +23,52 @@ public class QulicePlugin implements Plugin<Project> {
 
     public void runMavenDependencyAnalysis(Project prj) {
         /**
-         * @todo
+         * @todo #339:30min Implement Maven dependency analysis against a
+         *  Gradle project.
          **/
     }
     public void runMavenEnforcerPlugin(Project prj) {
         /**
-         * @todo
+         * @todo #339:30min Implement running of Maven enforcer plugin against
+         *  a Gradle project.
          **/
     }
     public void runJsLintMavenPlugin(Project prj) {
         /**
-         * @todo
+         * @todo #339:30min Implement running of jslint-maven-plugin against
+         *  this Gradle project.
          **/
     }
     public void runmavenDuplicateFinderPlugin(Project prj) {
         /**
-         * @todo
+         * @todo #339:30min Implement running of maven-duplicate-finder-plugin
+         *  against this Gradle project.
          **/
     }
     public void runCheckstyle(Project prj) {
         /**
-         * @todo
+         * @todo #339:30min Implement running Checkstyle checks on the Gradle
+         *  project.
          **/
     }
     public void runPMD(Project prj) {
         /**
-         * @todo
+         * @todo #339:30min Implement running PMD on this Gradle project.
          **/
     }
     public void runFindBugs(Project prj) {
         /**
-         * @todo
+         * @todo #339:30min Implement running FindBugs on this Gradle project.
          **/
     }
     public void runCobertura(Project prj) {
         /**
-         * @todo
+         * @todo #339:30min Implement running Cobertura on Gradle project.
          **/
     }
     public void runCodeNarc(Project prj) {
         /**
-         * @todo
+         * @todo #339:30min Implement running CodeNarc on Gradle project.
          **/
     }
 }
