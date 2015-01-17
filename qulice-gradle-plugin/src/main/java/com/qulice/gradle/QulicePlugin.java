1cecdeab90752cfadce16689505cb8e8dc62b5e5
Dmitri Pisarenko <dp@altruix.co>
2015-01-17 16:15:41 +0300

    Fixed the file as asked by Carlos Miranda.

--- qulice-gradle-plugin/src/main/java/com/qulice/gradle/QulicePlugin.java
+++ qulice-gradle-plugin/src/main/java/com/qulice/gradle/QulicePlugin.java
@@ -39,66 +39,9 @@ import org.gradle.api.Project;
  * @since 1.0
  */
 public final class QulicePlugin implements Plugin<Project> {
-    public void apply(Project project) {
-        this.runMavenDependencyAnalysis(project);
-        this.runMavenEnforcerPlugin(project);
-        this.runJsLintMavenPlugin(project);
-        this.runmavenDuplicateFinderPlugin(project);
-        this.runCheckstyle(project);
-        this.runPMD(project);
-        this.runFindBugs(project);
-        this.runCobertura(project);
-        this.runCodeNarc(project);
-    }
-
-    public void runMavenDependencyAnalysis(Project prj) {
+    public void apply(final Project project) {
         /**
-         * @todo #339:30min Implement Maven dependency analysis against a
-         *  Gradle project.
-         **/
-    }
-    public void runMavenEnforcerPlugin(Project prj) {
-        /**
-         * @todo #339:30min Implement running of Maven enforcer plugin against
-         *  a Gradle project.
-         **/
-    }
-    public void runJsLintMavenPlugin(Project prj) {
-        /**
-         * @todo #339:30min Implement running of jslint-maven-plugin against
-         *  this Gradle project.
-         **/
-    }
-    public void runmavenDuplicateFinderPlugin(Project prj) {
-        /**
-         * @todo #339:30min Implement running of maven-duplicate-finder-plugin
-         *  against this Gradle project.
-         **/
-    }
-    public void runCheckstyle(Project prj) {
-        /**
-         * @todo #339:30min Implement running Checkstyle checks on the Gradle
-         *  project.
-         **/
-    }
-    public void runPMD(Project prj) {
-        /**
-         * @todo #339:30min Implement running PMD on this Gradle project.
-         **/
-    }
-    public void runFindBugs(Project prj) {
-        /**
-         * @todo #339:30min Implement running FindBugs on this Gradle project.
-         **/
-    }
-    public void runCobertura(Project prj) {
-        /**
-         * @todo #339:30min Implement running Cobertura on Gradle project.
-         **/
-    }
-    public void runCodeNarc(Project prj) {
-        /**
-         * @todo #339:30min Implement running CodeNarc on Gradle project.
+         * @todo #339:30min Design validator classes for Gradle.
          **/
     }
 }
