49d1b4b67bfb133b5831bf1523b17b8cb171dc4f
Carlos Miranda <carlosmiranda@users.noreply.github.com>
2015-02-22 21:22:58 +0800

    #139 Verify that IndentationCheck bug is not present

---
+++ qulice-maven-plugin/src/it/checkstyle-violations/src/main/java/com/qulice/plugin/violations/IndentationChecks.java
@@ -0,0 +1,24 @@
+/**
+ * Test file.
+ */
+package com.qulice.plugin.violations;
+
+/**
+ * Indentation checks.
+ */
+public final class IndentationChecks {
+
+    /**
+     * This should not produce any violations.
+     */
+    public void triggersNoViolation() {
+        new String()
+            .substring(
+                0, 2
+            )
+            .substring(
+                0, 1
+            );
+    }
+
+}
