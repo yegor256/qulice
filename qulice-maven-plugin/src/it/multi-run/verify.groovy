31116f7f4227be528e17781d561f205fafc6eb5f
HDouss <douss.hamdi@gmail.com>
2015-12-29 22:40:25 +0100

    remove unnecessary comment

--- qulice-maven-plugin/src/it/multi-run/verify.groovy
+++ qulice-maven-plugin/src/it/multi-run/verify.groovy
@@ -32,7 +32,5 @@
  *
  */
 
-// Let's validate that the build never touched Groovy files
-// see ticket #39 for explanation
 def log = new File(basedir, 'build.log')
 assert log.text.contains('Finishing Checkstyle validator')
