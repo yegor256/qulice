ce9d728d822e1c4e15d2ce7eaaf723de83829260
Prahlad Yeri <prahladyeri@yahoo.com>
2016-01-06 23:35:41 +0530

    fixes #544; Added space to continuing lines of todo.

--- qulice-pmd/src/test/java/com/qulice/pmd/PMDEmptyTest.java
+++ qulice-pmd/src/test/java/com/qulice/pmd/PMDEmptyTest.java
@@ -46,8 +46,8 @@ import org.junit.Test;
  * @author Prahlad Yeri (prahladyeri@yahoo.com)
  * @version $Id$
  * @todo #544:30min Tests below pass only when run sequentially, when they are
- * run in parallel some of them start to fail. Please fix the tests below and
- * remove override of maven-surefire-plugin configuration in qulice-pmd pom.xml.
+ *  run in parallel some of them start to fail. Please fix the tests below and
+ *  remove override of maven-surefire-plugin configuration in qulice-pmd pom.xml.
  */
 @SuppressWarnings("PMD.TooManyMethods")
 public final class PMDEmptyTest {
