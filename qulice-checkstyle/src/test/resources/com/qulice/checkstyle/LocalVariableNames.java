a017f97a9e3d23c614cbd7f90243175c0329223e
jrdalpra <jrdalpra@gmail.com>
2016-01-16 11:57:25 -0200

    merge

--- qulice-checkstyle/src/test/resources/com/qulice/checkstyle/LocalVariableNames.java
+++ qulice-checkstyle/src/test/resources/com/qulice/checkstyle/LocalVariableNames.java
@@ -7,6 +7,7 @@ package foo;
  * Simple.
  * @version $Id$
  * @author John Smith (john@example.com)
+ * @checkstyle HiddenField (100 lines)
  * @since 1.0
  */
 public final class LocalVariableNames {
@@ -14,6 +15,10 @@ public final class LocalVariableNames {
      * Just a field.
      */
     private transient int field;
+    /**
+     * Just an id.
+     */
+    private transient int id;
 
     /**
      * Names that should not cause any violation.
@@ -21,7 +26,9 @@ public final class LocalVariableNames {
     void valid() {
         try {
             int aaa = this.field;
-            final int twelveletter = ++aaa;
+            int id = ++aaa;
+            final int ise = 0;
+            final int twelveletter = ++aaa + ++id;
         } catch (final IllegalStateException ise) {
             throw ise;
         } catch (final IllegalArgumentException ex) {
@@ -35,10 +42,12 @@ public final class LocalVariableNames {
     void invalid() {
         try {
             int prolongations = 0;
+            int very_long_variable_id = 0;
             int camelCase = this.field;
+            int it = 0;
             final int number1 = ++prolongations;
             final int ex = ++camelCase;
-            final int a = 0;
+            final int a = ++it + ++very_long_variable_id;
         } catch (final ArithmeticException ae) {
             throw ae;
         } catch (final IllegalArgumentException e) {
@@ -46,4 +55,3 @@ public final class LocalVariableNames {
         }
     }
 }
-
