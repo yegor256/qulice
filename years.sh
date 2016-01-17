31303ca7a214b5f31a3454702978256491303e58
krzyk <Krzysztof.Krason@gmail.com>
2016-01-17 19:10:38 +0100

    #583 make it bash

--- years.sh
+++ years.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 grep -L -r 2011-`date +%Y` --exclude-dir ".git" --exclude ".*" --exclude-dir "est" --exclude "*.yml" --exclude "*.md" --exclude-dir "target" --exclude-dir "it" --exclude-dir ".idea" --exclude "*.iml" | egrep -v "(qulice-pmd/src/test/resources|qulice-checkstyle/src/test/resources|qulice-xml/src/test/resources|src/site/resources/CNAME|qulice-codenarc/src/main/resources/com/qulice/codenarc/rules.txt|qulice-gradle-plugin/src/main/resources/META-INF/gradle-plugins/qulice-plugin.properties|years.sh)" 
 
