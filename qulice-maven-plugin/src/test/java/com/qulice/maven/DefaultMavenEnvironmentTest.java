b62853d5050e4d2e476d8ad5412e0bc5b575b138
Viktor Kuchyn <kuchin.victor@gmail.com>
2016-02-09 16:31:01 +0200

    #676: added mention to revert changes

--- qulice-maven-plugin/src/test/java/com/qulice/maven/DefaultMavenEnvironmentTest.java
+++ qulice-maven-plugin/src/test/java/com/qulice/maven/DefaultMavenEnvironmentTest.java
@@ -135,7 +135,8 @@ public class DefaultMavenEnvironmentTest {
      * @throws Exception If something wrong happens inside
      * @todo #676:30min Ideally integration test is required to reproduce NPE
      *  in issue #676.
-     *  To reproduce problem - add to pom.xml at least one exclude rule in
+     *  To reproduce problem firstly revert changes marked with #676, than
+     *  add to pom.xml of it project at least one exclude rule in
      *  configuration.excludes block and dependency (in my case worked
      *  org.hibernate:hibernate-annotations:3.5.6-Final).
      *  Suggestion is that dependency must be compile-time scope as far as
