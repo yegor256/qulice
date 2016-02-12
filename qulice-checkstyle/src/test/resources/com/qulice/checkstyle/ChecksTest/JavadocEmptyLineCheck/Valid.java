3cb5f0275d8f5d08e8be7eba60c38f6089502ccc
DSkalenko <d.skalenko@gmail.com>
2016-02-12 12:08:11 +0100

    #656 Prohibit empty line at the start and at the end of javadoc: fix code review remarks

--- qulice-checkstyle/src/test/resources/com/qulice/checkstyle/ChecksTest/JavadocEmptyLineCheck/Valid.java
+++ qulice-checkstyle/src/test/resources/com/qulice/checkstyle/ChecksTest/JavadocEmptyLineCheck/Valid.java
@@ -4,60 +4,115 @@
  */
 public final class Valid {
 
+    /**
+     * There is no empty Javadoc line at the beginning.
+     */
+    private static final int x = 0;
 
-	private int x = 0;
+    /**
+     * There is no empty Javadoc line at the beginning.
+     */
+    private int y = 0;
 
-	/**
-	 * empty javadoc line at the beginning
-	 */
-	private int y = 0;
+    /**
+     * There is no empty Javadoc line at the end.
+     */
+    private int z = 0;
 
-	/**
-	 * empty javadoc line at the end
-	 */
-	private int z = 0;
+    /**
+     * There is no empty Javadoc line at the beginning.
+     */
+    public Valid() {}
 
-	/**
-	 * empty javadoc line at the beginning
-	 */
-	public Valid() {}
+    /**
+     *
+     * There is no empty Javadoc line
+     * @param param - param.
+     * @return Param.
+     *
+     */
+    public String method(final String param) {
+        return param;
+    }
 
-	/**
-	 * empty javadoc line at the end
-	 */
-	public void method() {
-	}
+    /*
+     *
+     * It's not Javadoc, just some comment
+     */
+    public void method2() {
+    }
 
-	/**
-	 * empty javadoc line at the beginning
-	 */
-	public void method2() {
-	}
+    /**
+     * First.
+     *
+     * Second.
+     */
+    public void method3() {
+    }
 
-	/**
-	 * empty javadoc line at the beginning
-	 */
-	public void method3() {
-	}
+    private static class InnerValid {
 
-	public void method4() {
-	}
+        /**
+         * There is no empty Javadoc line at the end.
+         */
+        private final int x;
+
+        /**
+         * There is no empty Javadoc line at the beginning.
+         * @param x - x
+         */
+        public InnerValid(int x) {
+            this.x = x;
+        }
+
+        /**
+         * There is no empty Javadoc line at the beginning.
+         */
+        public void method() {
+        }
+    }
 }
+
 /**
- * This is not a real Java class. It won't be compiled ever. It is used
+ * This is not a real interface. It won't be compiled ever. It is used
  * only as a text resource in integration.ChecksIT.
  */
 interface I1 {
-	public void method3() {
-	}
 
-	/**
-	 * Integer variable.
-	 */
-	private int x = 0;
+    /**
+     * There is no empty Javadoc line at the beginning
+     */
+    void method();
+
+    /**
+     * Integer variable.
+     */
+    int x = 0;
 
 }
 
-interface I2 {
+/**
+ * There is no empty Javadoc line.
+ */
+enum ValidEnum{
 
+    /**
+     * There is no empty Javadoc line at the beginning.
+     */
+    VALID_ENUM("someValue");
+
+    /**
+     * There is no empty Javadoc line at the end.
+     */
+    private final String name;
+
+
+    /**
+     * There is no empty Javadoc line at the end.
+     */
+    ValidEnum(final String name) {
+        this.name = name;
+    }
 }
+
+
