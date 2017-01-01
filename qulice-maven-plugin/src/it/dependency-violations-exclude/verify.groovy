348ebfe55fce49a2d9445faf0a065afad245c8c1
Yegor Bugayenko <yegor256@gmail.com>
2017-01-01 21:45:17 +0200

    year license up

--- qulice-maven-plugin/src/it/dependency-violations-exclude/verify.groovy
+++ qulice-maven-plugin/src/it/dependency-violations-exclude/verify.groovy
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (c) 2011-2016, Qulice.com
+ * Copyright (c) 2011-2017, Qulice.com
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
