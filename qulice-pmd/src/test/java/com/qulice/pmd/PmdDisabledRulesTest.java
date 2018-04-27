2288e88dec3f40daf0a3c386ad253c47cfa14af8
krzyk <Krzysztof.Krason@gmail.com>
2018-04-27 17:20:25 +0200

    #869 fixing copyright years

--- qulice-pmd/src/test/java/com/qulice/pmd/PmdDisabledRulesTest.java
+++ qulice-pmd/src/test/java/com/qulice/pmd/PmdDisabledRulesTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2011-2017, Qulice.com
+ * Copyright (c) 2011-2018, Qulice.com
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
