2cf2162d161a755866c281ccb3050cb0f92d48c1
krzyk <Krzysztof.Krason@gmail.com>
2018-12-22 17:28:04 +0100

    #963 remove requirement for serial version UID

--- qulice-pmd/src/test/java/com/qulice/pmd/PmdDisabledRulesTest.java
+++ qulice-pmd/src/test/java/com/qulice/pmd/PmdDisabledRulesTest.java
@@ -70,6 +70,7 @@ public final class PmdDisabledRulesTest {
                 {"DefaultPackage"},
                 {"ExcessiveImports"},
                 {"PositionLiteralsFirstInComparisons"},
+                {"MissingSerialVersionUID"},
             }
         );
     }
