916e850b5014b524e086e8c8d8dc44cf40d02a0f
Yegor Bugayenko <yegor256@gmail.com>
2021-10-01 20:32:57 +0300

    #1097 years off

--- years.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if (grep -L -r 2011-`date +%Y` --exclude-dir ".git" --exclude ".*" --exclude-dir "est" --exclude "*.yml" --exclude "*.md" --exclude-dir "target" --exclude-dir "it" . | egrep -v "(qulice-pmd/src/test/resources|qulice-checkstyle/src/test/resources|src/site/resources/CNAME|years.sh)"); then
-    echo "Files above have wrong years in copyrights"
-    exit 1
-fi
-
