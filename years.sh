c32afb94ca335bbb39746f891c7db29106ce6a7f
Yegor Bugayenko <yegor256@gmail.com>
2021-10-01 19:47:13 +0300

    #1097 gradle off

--- years.sh
+++ years.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if (grep -L -r 2011-`date +%Y` --exclude-dir ".git" --exclude ".*" --exclude-dir "est" --exclude "*.yml" --exclude "*.md" --exclude-dir "target" --exclude-dir "it" . | egrep -v "(qulice-pmd/src/test/resources|qulice-checkstyle/src/test/resources|src/site/resources/CNAME|qulice-gradle-plugin/src/main/resources/META-INF/gradle-plugins/qulice-plugin.properties|years.sh)"); then
+if (grep -L -r 2011-`date +%Y` --exclude-dir ".git" --exclude ".*" --exclude-dir "est" --exclude "*.yml" --exclude "*.md" --exclude-dir "target" --exclude-dir "it" . | egrep -v "(qulice-pmd/src/test/resources|qulice-checkstyle/src/test/resources|src/site/resources/CNAME|years.sh)"); then
     echo "Files above have wrong years in copyrights"
     exit 1
 fi
