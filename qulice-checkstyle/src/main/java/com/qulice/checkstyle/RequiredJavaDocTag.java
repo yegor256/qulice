8c8068c5466b06023c50ef4bcba3a00deac7a845
Vladimir.Shapkin <Vladimir.Shapkin@x5.ru>
2024-07-04 17:42:09 +0400

    Fixed different behavior between @param and @since The @param regex allows varying numbers of spaces before and after the @param tag, while the @since tag only allows one space before and one after it.

---
+++ qulice-checkstyle/src/main/java/com/qulice/checkstyle/RequiredJavaDocTag.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright (c) 2011-2024 Qulice.com
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the Qulice.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.qulice.checkstyle;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Check the required JavaDoc tag in the lines.
+ * <p>Correct format is the following (of a class javadoc):
+ *
+ * <pre>
+ * &#47;**
+ *  * This is my new class.
+ *  *
+ *  * &#64;since 0.3
+ *  *&#47;
+ * public final class Foo {
+ *     &#47;**
+ *      * This is my other class.
+ *      *
+ *      *    &#64;since    0.3
+ *      *&#47;
+ *     public final class Boo {
+ *     // ...
+ * </pre>
+ *
+ * <p>"&#36;Id&#36;" will be replaced by a full text automatically
+ * by Subversion as explained in their documentation (see link below).
+ *
+ * @see <a href="http://svnbook.red-bean.com/en/1.4/svn.advanced.props.special.keywords.html">Keywords substitution in Subversion</a>
+
+ * @since 0.23.1
+ */
+final class RequiredJavaDocTag {
+    /**
+     * Tag name.
+     */
+    private final String name;
+
+    /**
+     * Pattern for searching a tag in a string.
+     */
+    private final Pattern tag;
+
+    /**
+     * Pattern for checking the contents of a tag in a string.
+     */
+    private final Pattern content;
+
+    /**
+     * Reference to a method for writing a message to the log.
+     */
+    private final LogWriter writer;
+
+    /**
+     * Ctor.
+     * @param name Tag name.
+     * @param patt Pattern for checking the contents of a tag in a string.
+     * @param lwriter Reference to a method for writing a message to the log.
+     */
+    RequiredJavaDocTag(final String name, final Pattern patt,
+        final LogWriter lwriter) {
+        this(
+            name,
+            Pattern.compile(
+                String.format(
+                    "(?<name>^ +\\* +@%s)( +)(?<cont>.*)",
+                    name
+                )
+            ),
+            patt,
+            lwriter
+        );
+    }
+
+    /**
+     * Ctor.
+     * @param cname Tag name.
+     * @param ptag Pattern for searching a tag in a string.
+     * @param patt Pattern for checking the contents of a tag in a string.
+     * @param lwriter Reference to a method for writing a message to the log.
+     * @checkstyle ParameterNumberCheck (3 lines)
+     */
+    RequiredJavaDocTag(final String cname, final Pattern ptag,
+        final Pattern patt, final LogWriter lwriter) {
+        this.name = cname;
+        this.tag = ptag;
+        this.content = patt;
+        this.writer = lwriter;
+    }
+
+    /**
+     * Check if the tag text matches the format from pattern.
+     * @param lines List of all lines.
+     * @param start Line number where comment starts.
+     * @param end Line number where comment ends.
+     */
+    public void matchTagFormat(final String[] lines, final int start,
+        final int end) {
+        final Map<Integer, String> found = new HashMap<>(1);
+        for (int pos = start; pos <= end; pos += 1) {
+            final String line = lines[pos];
+            final Matcher matcher = this.tag.matcher(line);
+            if (matcher.matches()
+                && !RequiredJavaDocTag.empty(matcher.group("name"))
+                && !RequiredJavaDocTag.empty(matcher.group("cont"))
+            ) {
+                found.put(pos, matcher.group("cont"));
+                break;
+            }
+        }
+        if (found.isEmpty()) {
+            this.writer.log(
+                start + 1,
+                "Missing ''@{0}'' tag in class/interface comment",
+                this.name
+            );
+        } else {
+            for (final Map.Entry<Integer, String> item : found.entrySet()) {
+                if (!this.content.matcher(item.getValue()).matches()) {
+                    this.writer.log(
+                        item.getKey() + 1,
+                        "Tag text ''{0}'' does not match the pattern ''{1}''",
+                        item.getValue(),
+                        this.content.toString()
+                    );
+                }
+            }
+        }
+    }
+
+    /**
+     * Checks for an empty string.
+     * @param str Line to check.
+     * @return True if str is empty.
+     */
+    private static boolean empty(final String str) {
+        return str == null || str.isBlank();
+    }
+
+    /**
+     * Logger.
+     * @see com.puppycrawl.tools.checkstyle.api.AbstractCheck.log
+     * @since 0.23.1
+     */
+    interface LogWriter {
+        /**
+         * Log a message that has no column information.
+         *
+         * @param line The line number where the audit event was found.
+         * @param msg The message that describes the audit event.
+         * @param args The details of the message.
+         * @see java.text.MessageFormat
+         */
+        void log(int line, String msg, Object... args);
+    }
+}
---
+++ qulice-checkstyle/src/test/java/com/qulice/checkstyle/RequiredJavaDocTagTest.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2011-2024 Qulice.com
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the Qulice.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.qulice.checkstyle;
+
+import java.text.MessageFormat;
+import java.util.regex.Pattern;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test case for {@link RequiredJavaDocTag} class.
+ * @since 0.23.1
+ */
+final class RequiredJavaDocTagTest {
+
+    /**
+     * Logger.
+     */
+    private final WriterStub writer = new WriterStub();
+
+    /**
+     * Object under test.
+     */
+    private final RequiredJavaDocTag tag = new RequiredJavaDocTag(
+        "since",
+        Pattern.compile(
+            "^\\d+(\\.\\d+){1,2}(\\.[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?$"
+        ),
+        this.writer
+    );
+
+    @Test
+    void success() {
+        final String[] lines = {" *", " * @since 0.3", " *"};
+        this.tag.matchTagFormat(lines, 0, 2);
+        MatcherAssert.assertThat(
+            "Empty string expected",
+            this.writer.formattedMessage(),
+            Matchers.emptyString()
+        );
+    }
+
+    @Test
+    void successWithSpaces() {
+        final String[] lines = {" *", "    *    @since    0.3", " *"};
+        this.tag.matchTagFormat(lines, 0, 2);
+        MatcherAssert.assertThat(
+            "Empty string expected",
+            this.writer.formattedMessage(),
+            Matchers.emptyString()
+        );
+    }
+
+    @Test
+    void tagNotFound() {
+        final String[] lines = {" *", " * @sinc 0.3", " *"};
+        this.tag.matchTagFormat(lines, 0, 2);
+        MatcherAssert.assertThat(
+            "Expected tag not found",
+            this.writer.formattedMessage(),
+            Matchers.equalTo("Missing '@since' tag in class/interface comment")
+        );
+    }
+
+    @Test
+    void tagNotMatched() {
+        final String[] lines = {" *", " * @since 0.3.4.4.", " *"};
+        this.tag.matchTagFormat(lines, 0, 2);
+        MatcherAssert.assertThat(
+            "Regular expression non-match expected",
+            this.writer.formattedMessage(),
+            Matchers.equalTo(
+                String.format(
+                    "Tag text '0.3.4.4.' does not match the pattern '%s'",
+                    "^\\d+(\\.\\d+){1,2}(\\.[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?$"
+                )
+            )
+        );
+    }
+
+    /**
+     * Stub for {@link RequiredJavaDocTag.LogWriter} class.
+     * @since 0.23.1
+     */
+    private static class WriterStub implements RequiredJavaDocTag.LogWriter {
+
+        /**
+         * Message.
+         */
+        private String message;
+
+        /**
+         * Ctor.
+         */
+        WriterStub() {
+            this.message = "";
+        }
+
+        @Override
+        public void log(final int line, final String msg, final Object... args) {
+            this.message = MessageFormat.format(msg, args);
+        }
+
+        String formattedMessage() {
+            return this.message;
+        }
+    }
+}
