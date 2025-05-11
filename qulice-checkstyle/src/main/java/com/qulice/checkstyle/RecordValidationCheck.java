5fa60e087cd1ace5d92c990223ffea5156d9b8c1
AruzhanShinbayeva <a.shinbaeyeva@innopolis.university>
2025-05-12 01:39:30 +0300

    fix issues

--- qulice-checkstyle/src/main/java/com/qulice/checkstyle/RecordValidationCheck.java
+++ qulice-checkstyle/src/main/java/com/qulice/checkstyle/RecordValidationCheck.java
@@ -1,3 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2011-2025 Yegor Bugayenko
+ * SPDX-License-Identifier: MIT
+ */
 package com.qulice.checkstyle;
 
 import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
@@ -11,8 +15,9 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
  * 2. Record components are properly formatted
  * 3. Records do not extend other classes
  * 4. Records are final
+ * @since 0.24
  */
-public class RecordValidationCheck extends AbstractCheck {
+public final class RecordValidationCheck extends AbstractCheck {
     /**
      * A key is pointing to the warning message text in "messages.properties"
      * file.
@@ -22,79 +27,94 @@ public class RecordValidationCheck extends AbstractCheck {
     @Override
     public int[] getDefaultTokens() {
         return new int[] {
-                TokenTypes.RECORD_DEF,
-                TokenTypes.RECORD_COMPONENT_DEF
+            TokenTypes.RECORD_DEF,
+            TokenTypes.RECORD_COMPONENT_DEF,
         };
     }
 
     @Override
     public int[] getAcceptableTokens() {
-        return getDefaultTokens();
+        return this.getDefaultTokens();
     }
 
     @Override
     public int[] getRequiredTokens() {
-        return getDefaultTokens();
+        return this.getDefaultTokens();
     }
 
     @Override
-    public void visitToken(DetailAST ast) {
+    public void visitToken(final DetailAST ast) {
         switch (ast.getType()) {
             case TokenTypes.RECORD_DEF:
-                checkRecordDeclaration(ast);
+                this.checkRecordDeclaration(ast);
                 break;
             case TokenTypes.RECORD_COMPONENT_DEF:
-                checkRecordComponent(ast);
+                this.checkRecordComponent(ast);
                 break;
+            default:
         }
     }
 
-    private void checkRecordDeclaration(DetailAST ast) {
-        // Check if record extends another class
+    /**
+     * Check if record extends another class.
+     * Check if record is final.
+     * Check if record has components.
+     * @param ast EXPR RECORD_DEF node that needs to be checked
+     */
+    private void checkRecordDeclaration(final DetailAST ast) {
         if (ast.findFirstToken(TokenTypes.EXTENDS_CLAUSE) != null) {
-            log(ast.getLineNo(), ast.getColumnNo(), "Records cannot extend other classes");
+            this.log(ast.getLineNo(), ast.getColumnNo(), "Records cannot extend other classes");
         }
-
-        // Check if record is final
-        DetailAST modifiers = ast.findFirstToken(TokenTypes.MODIFIERS);
+        final DetailAST modifiers = ast.findFirstToken(TokenTypes.MODIFIERS);
         if (modifiers != null && modifiers.findFirstToken(TokenTypes.FINAL) == null) {
-            log(modifiers.getLineNo(), modifiers.getColumnNo(), "Records must be final");
+            this.log(modifiers.getLineNo(), modifiers.getColumnNo(), "Records must be final");
         }
-
-        // Check if record has components
-        DetailAST components = ast.findFirstToken(TokenTypes.RECORD_COMPONENTS);
-        if (components != null && components.findFirstToken(TokenTypes.RECORD_COMPONENT_DEF) == null) {
-            log(components.getLineNo(), components.getColumnNo(), "Records must declare at least one component");
+        final DetailAST components = ast.findFirstToken(TokenTypes.RECORD_COMPONENTS);
+        if (components != null
+            && components.findFirstToken(TokenTypes.RECORD_COMPONENT_DEF) == null) {
+            this.log(
+                components.getLineNo(),
+                components.getColumnNo(),
+                "Records must declare at least one component"
+            );
         }
-
-        checkRecordInstanceFields(ast);
+        this.checkRecordInstanceFields(ast);
     }
 
-    private void checkRecordInstanceFields(DetailAST ast) {
-        DetailAST objBlockAst = ast.findFirstToken(TokenTypes.OBJBLOCK);
-
-        // Traverse the children of the objBlockAst to find variable definitions
-        for (DetailAST child = objBlockAst.getFirstChild(); child != null; child = child.getNextSibling()) {
+    /**
+     * Check record does not contain instance field.
+     * @param ast EXPR RECORD_DEF node that needs to be checked
+     */
+    private void checkRecordInstanceFields(final DetailAST ast) {
+        final DetailAST block = ast.findFirstToken(TokenTypes.OBJBLOCK);
+        DetailAST child = block.getFirstChild();
+        while (child != null) {
             if (child.getType() == TokenTypes.VARIABLE_DEF) {
-                DetailAST modifiersAst = child.findFirstToken(TokenTypes.MODIFIERS);
-                DetailAST isStatic = modifiersAst.findFirstToken(TokenTypes.LITERAL_STATIC);
-
-                if (isStatic == null) {
-                    log(child.getLineNo(), child.getColumnNo(), "Records cannot have instance fields");
+                final DetailAST modifiers = child.findFirstToken(TokenTypes.MODIFIERS);
+                final DetailAST statics = modifiers.findFirstToken(TokenTypes.LITERAL_STATIC);
+                if (statics == null) {
+                    this.log(
+                        child.getLineNo(),
+                        child.getColumnNo(),
+                        "Records cannot have instance fields"
+                    );
                 }
             }
+            child = child.getNextSibling();
         }
     }
 
-    private void checkRecordComponent(DetailAST ast) {
-        // Check if component has a type
+    /**
+     * Check if component has a type.
+     * Check if component has a name.
+     * @param ast EXPR RECORD_COMPONENT_DEF node that needs to be checked
+     */
+    private void checkRecordComponent(final DetailAST ast) {
         if (ast.findFirstToken(TokenTypes.TYPE) == null) {
-            log(ast.getLineNo(), ast.getColumnNo(), "Record component must have a type");
+            this.log(ast.getLineNo(), ast.getColumnNo(), "Record component must have a type");
         }
-
-        // Check if component has a name
         if (ast.findFirstToken(TokenTypes.IDENT) == null) {
-            log(ast.getLineNo(), ast.getColumnNo(), "Record component must have a name");
+            this.log(ast.getLineNo(), ast.getColumnNo(), "Record component must have a name");
         }
     }
 }
