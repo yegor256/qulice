c5eabae76ea194c60a78ec32b2f2d5910da07a57
Dema-koder <asotruzikovic@gmail.com>
2025-05-15 07:55:58 +0300

    partially fix typos

--- qulice-pmd/src/test/resources/com/qulice/pmd/CallToConstructorInConstructor.java
+++ qulice-pmd/src/test/resources/com/qulice/pmd/CallToConstructorInConstructor.java
@@ -22,7 +22,7 @@ public final class CallToConstructorInConstructor extends Super {
         return number;
     }
 
-    public int another() {
+    public int anothe() {
         return another;
     }
 }
