26ced55af167641277fbf8837f0870e51d9b3c8c
Yegor Bugayenko <yegor256@gmail.com>
2025-10-01 08:23:01 +0300

    #1221: Update dependencies versions and remove unnecessary files.

--- qulice-maven-plugin/src/it/duplicate-finder-violations/verify.groovy
+++
@@ -1,9 +0,0 @@
-/**
- *
- * SPDX-FileCopyrightText: Copyright (c) 2011-2025 Yegor Bugayenko
- * SPDX-License-Identifier: MIT
- */
-
-def log = new File(basedir, 'build.log')
-assert log.text.contains('Found duplicate and different classes in ' +
-    '[org.apache.xmlgraphics:batik-ext:1.7, xml-apis:xml-apis:1.4.01]')
