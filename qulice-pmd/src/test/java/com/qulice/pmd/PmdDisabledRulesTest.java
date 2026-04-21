b38962f9e944e40439deb9c85f583800bc1d6c1c
Yegor Bugayenko <yegor256@gmail.com>
2026-04-21 17:14:32 +0300

    #1527: Exclude AvoidUsingHardCodedIP rule.

--- qulice-pmd/src/test/java/com/qulice/pmd/PmdDisabledRulesTest.java
+++ qulice-pmd/src/test/java/com/qulice/pmd/PmdDisabledRulesTest.java
@@ -43,6 +43,7 @@ final class PmdDisabledRulesTest {
                 {"PositionLiteralsFirstInComparisons"},
                 {"MissingSerialVersionUID"},
                 {"CallSuperInConstructor"},
+                {"AvoidUsingHardCodedIP"},
             }
         );
     }
---
+++ qulice-pmd/src/test/resources/com/qulice/pmd/AvoidUsingHardCodedIP.java
@@ -0,0 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2011-2026 Yegor Bugayenko
+ * SPDX-License-Identifier: MIT
+ */
+package foo;
+
+public final class AvoidUsingHardCodedIP {
+    private final String version = "0.0.0.43";
+}
