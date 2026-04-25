ac662e61e5295fd3ef9bc0502681e9e0000282f9
Yegor Bugayenko <yegor256@gmail.com>
2026-04-25 20:43:23 +0300

    Update dependencies versions and fix verification script.

--- src/it/dependency-not-matches-exclude/verify.groovy
+++ src/it/dependency-not-matches-exclude/verify.groovy
@@ -6,5 +6,5 @@
 def log = new File(basedir, 'build.log')
 assert !log.text.contains('Caused by: java.lang.NullPointerException')
 assert log.text.contains('Unused declared dependencies found')
-assert log.text.contains('1 dependency problem(s) found')
-assert log.text.contains('org.hibernate:hibernate-entitymanager:jar:5.0.7.Final:compile')
+assert log.text.contains('2 dependency problem(s) found')
+assert log.text.contains('org.hibernate:hibernate-entitymanager:jar:5.6.15.Final:compile')
